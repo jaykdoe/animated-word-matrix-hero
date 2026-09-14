9212bc300bbe7efff7c000df30cea1362c4d6ccb
jaykdoe <jaykdoe@gmail.com>
2026-09-14 14:36:10 -0700

    /

--- lib/local-storage.ts
+++ lib/local-storage.ts
@@ -1,27 +1,32 @@
 const addToLocalStorage = (key: string, value: any) => {
-    // if (typeof window !== 'undefined') {
+    if (typeof window !== 'undefined') {
         localStorage?.setItem(key, JSON.stringify(value));
-    // }
-
+    }  else {
+        console.log('localStorage is not available');
+    }
 };
 
 // Usage:
 // addToLocalStorage('user', { name: 'John', age: 30 });
 
 const isItemInLocalStorage = (key: string) => {
-    // if (typeof window !== 'undefined') {
+    if (typeof window !== 'undefined') {
          return localStorage?.getItem(key) !== null;
-    // }
+    }  else {
+        console.log('localStorage is not available');
+    }
 };
 
 // Usage:
 // console.log(isItemInLocalStorage('user')); // Output: true or false
 
 const getFromLocalStorage = (key: string) => {
-    // if (typeof window !== 'undefined') {
+    if (typeof window !== 'undefined') {
         const item = localStorage?.getItem(key);
         return item ? item : null;
-    // }
+    }  else {
+        console.log('localStorage is not available');
+    }
 };
 
 // Usage:
